7c8a2c03b24e5e16713cf0f0c1196680890796a8
ramprakashpg <ramprakash.pg@thoughtworks.com>
2022-03-16 15:18:34 +0530

    [No-Cards]|Ram/Sahiti| Added Logic to change the content on a button click

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,20 @@
-import { Component, Input } from '@angular/core';
-
+import { Component, NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button'
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 
+
 export class AppComponent {
-  // @Input()
-  // heading!:string;
+
   title = 'XAct_Frontend_App';  
   heading1 = "Hello world"
+
+  changeContent(){
+    this.heading1 = "Welcome"
+  }
 
 }
 
