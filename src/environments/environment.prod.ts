d7d2cc70cade42ddbf310aeb9a7b564f8d6e0f27
Shashank Mishra10 <smishra10@lenovo.com>
2022-03-28 12:43:45 +0530

    [No-Card] [Shashank/NA] | Moving env variables to env files

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,7 @@
 export const environment = {
   production: true,
   URL:"https://api.project-xact.in",
+  ISSUER: "https://fake-issuer",
+  CLIENT_ID: "fake-id",
+  OKTA_TESTING_DISABLEHTTPSCHECK: false
 };
