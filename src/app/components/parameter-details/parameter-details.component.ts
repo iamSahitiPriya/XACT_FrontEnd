96c9004baac65470f6c6415502644fb586108657
RUCHIKA MODGIL <ruchika.modgil@thoughtworks.com>
2022-05-17 10:52:39 +0530

    [30] [Ruchika | Sahiti] Styling Modifications on Parameter Assessment

--- src/app/components/parameter-details/parameter-details.component.ts
+++ src/app/components/parameter-details/parameter-details.component.ts
@@ -1,9 +1,6 @@
 import {Component, Input} from '@angular/core';
-import {AssessmentStructure} from "../../types/assessmentStructure";
-import {CategoryStructure} from "../../types/categoryStructure";
 import {TopicStructure} from "../../types/topicStructure";
 import {ParameterStructure} from "../../types/parameterStructure";
-import {AppServiceService} from "../../services/app-service/app-service.service";
 
 
 @Component({
@@ -17,6 +14,5 @@ export class ParameterDetailsComponent {
 
   @Input()
   topicInput: TopicStructure;
-
 
 }
