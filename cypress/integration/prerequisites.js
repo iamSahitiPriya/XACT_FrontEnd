fec097d3b36478e2b794435be1822a14b1030f6b
JathinMeduri <102661843+JathinMeduri@users.noreply.github.com>
2022-06-02 09:03:48 +0530

    [5][6][Jathin]implemented test files for login,assessment,search functionality,updated locators,prerequisites

--- cypress/integration/prerequisites.js
+++ cypress/integration/prerequisites.js
@@ -1,39 +1,82 @@
 import loginPage from "./pageObjects/loginPage";
 import landingPage from "./pageObjects/landingPage";
+//import {waitForAngularReady} from "@angular/cdk/testing/selenium-webdriver";
 
 describe('completing prerequisites to test the application', () => {
-  before('User should get navigated to Okta by launching the url', () => {
+  beforeEach('User should get navigated to Okta by launching the url', () => {
+    cy.window().then(win => win.location.hash = "/foo/bar")
+    cy.intercept('GET','https://dev-47045452.okta.com/oauth2/default/v1/userinfo').as('pageLoad')
     cy.visit('/')
   })
 
-  // it('tc001 user tries to login  with valid userId and password',()=>{
-  //   loginPage.xActLogin('technicalbaba4u@gmail.com','Sam@12345')
-  //   loginPage.xActHomepagetitleValidation()
-  // })
+  it('tc001 user tries to login  with valid userId and password',()=>{
+    loginPage.xActLogin('technicalbaba4u@gmail.com','Sam@12345')
+    loginPage.xActHomepagetitleValidation()
+  })
 
   it('Creating 30 assessments for testing purpose',()=>{
-    for (var iCount = 1; iCount <= 3; iCount++) {
+    for (var iCount = 1; iCount <= 20; iCount++) {
+      cy.intercept('GET','https://dev-47045452.okta.com/oauth2/default/v1/userinfo').as('pageLoad')
+      cy.wait('@pageLoad')
+      landingPage.createAssessment().click()
       landingPage.AssessmentpopupFields('TestAssessment'+iCount,'TestOrganisation'+iCount,'TestDomain'+iCount,'TestIndustry',iCount,'test@thoughtworks.com')
       landingPage.saveAssessmentButton().click()
+
     }
   })
   it('search prerequisites',()=>{
+    cy.intercept('GET','https://dev-47045452.okta.com/oauth2/default/v1/userinfo').as('pageLoad')
+    cy.wait('@pageLoad')
+    //waitForAngularReady()
+    landingPage.createAssessment().click()
       landingPage.AssessmentpopupFields('searchAssessment','searchOrganisation','TestDomain','TestIndustry','22','test@thoughtworks.com')
     landingPage.saveAssessmentButton().click()
+    landingPage.createAssessment().click()
       landingPage.AssessmentpopupFields('searchAssessment','searchOrganisation','TestDomain','TestIndustry','22','test@thoughtworks.com')
     landingPage.saveAssessmentButton().click()
+    landingPage.createAssessment().click()
     landingPage.AssessmentpopupFields('abcd','mno','TestDomain','TestIndustry','22','test@thoughtworks.com')
     landingPage.saveAssessmentButton().click()
+    landingPage.createAssessment().click()
     landingPage.AssessmentpopupFields('qrst','abc','TestDomain','TestIndustry','22','test@thoughtworks.com')
     landingPage.saveAssessmentButton().click()
+    landingPage.createAssessment().click()
     landingPage.AssessmentpopupFields('xact','thoughtworks','TestDomain','TestIndustry','22','test@thoughtworks.com')
     landingPage.saveAssessmentButton().click()
+    landingPage.createAssessment().click()
     landingPage.AssessmentpopupFields('hi hello','how are you','TestDomain','TestIndustry','22','test@thoughtworks.com')
     landingPage.saveAssessmentButton().click()
+    landingPage.createAssessment().click()
     landingPage.AssessmentpopupFields('devTeam','BaTeam','TestDomain','TestIndustry','22','test@thoughtworks.com')
     landingPage.saveAssessmentButton().click()
   })
 
+  it('search prerequisites',()=>{
+    cy.intercept('GET','https://dev-47045452.okta.com/oauth2/default/v1/userinfo').as('pageLoad')
+    cy.wait('@pageLoad')
+    //waitForAngularReady()
+    landingPage.createAssessment().click()
+    landingPage.AssessmentpopupFields('searchAssessment','searchOrganisation','TestDomain','TestIndustry','22','test@thoughtworks.com')
+    landingPage.saveAssessmentButton().click()
+    landingPage.createAssessment().click()
+    landingPage.AssessmentpopupFields('searchAssessment','searchOrganisation','TestDomain','TestIndustry','22','test@thoughtworks.com')
+    landingPage.saveAssessmentButton().click()
+    landingPage.createAssessment().click()
+    landingPage.AssessmentpopupFields('abcd','mno','TestDomain','TestIndustry','22','test@thoughtworks.com')
+    landingPage.saveAssessmentButton().click()
+    landingPage.createAssessment().click()
+    landingPage.AssessmentpopupFields('qrst','abc','TestDomain','TestIndustry','22','test@thoughtworks.com')
+    landingPage.saveAssessmentButton().click()
+    landingPage.createAssessment().click()
+    landingPage.AssessmentpopupFields('xact','thoughtworks','TestDomain','TestIndustry','22','test@thoughtworks.com')
+    landingPage.saveAssessmentButton().click()
+    landingPage.createAssessment().click()
+    landingPage.AssessmentpopupFields('hi hello','how are you','TestDomain','TestIndustry','22','test@thoughtworks.com')
+    landingPage.saveAssessmentButton().click()
+    landingPage.createAssessment().click()
+    landingPage.AssessmentpopupFields('devTeam','BaTeam','TestDomain','TestIndustry','22','test@thoughtworks.com')
+    landingPage.saveAssessmentButton().click()
+  })
 
 
 
