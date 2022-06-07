8b2b53707e655b6dfd525472cd588d8a3e1f6c28
Shashank Mishra10 <smishra10@lenovo.com>
2022-06-07 10:35:50 +0530

    [60] [Shashank] | Add QA deploy step

---
+++ src/environments/environment.qa.ts
@@ -0,0 +1,31 @@
+/*
+ * Copyright (c) 2022 - Thoughtworks Inc. All rights reserved.
+ */
+
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  BaseURI:"https://api-qa.project-xact.in",
+  ISSUER: '{ISSUER}',
+  CLIENT_ID: '{CLIENT_ID}',
+  REDIRECT_URI:"https://qa.project-xact.in/login/callback",
+  OKTA_TESTING_DISABLEHTTPSCHECK: false,
+  ASSESSMENT_URI:"/v1/assessments",
+  USER_URI: "/v1/users",
+  CATEGORY_URI:"/v1/assessment-master-data/categories",
+  SAVE_ASSESSMENT_URI: "/v1/notes",
+  ASSESSMENT_REPORT_URI: "/v1/reports/assessments/",
+  ASSESSMENT_STATUS_FINISH_URI: "/statuses/finish",
+  ASSESSMENT_STATUS_OPEN_URI: "/statuses/open",
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
