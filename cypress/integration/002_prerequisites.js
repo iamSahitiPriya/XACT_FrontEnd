cd75a4e7af7761c7258d93e6cbe31faf56275048
JathinMeduri <102661843+JathinMeduri@users.noreply.github.com>
2022-08-05 11:16:43 +0530

    [Jathin] added/updated TC

--- cypress/integration/002_prerequisites.js
+++ cypress/integration/002_prerequisites.js
@@ -16,24 +16,30 @@ describe('completing prerequisites to test the application', () => {
   //   loginPage.xActHomepagetitleValidation()
   // })
 
-  it('Creating 30 assessments for testing purpose',()=>{
+  it('Creating 30 assessments for testing purpose',()=> {
     for (var iCount = 1; iCount <= 40; iCount++) {
       landingPage.clickCreateAssessment()
-      landingPage.AssessmentpopupFields('TestAssessment'+iCount,'TestOrganisation'+iCount,'TestDomain'+iCount,'TestIndustry',iCount,'test@thoughtworks.com')
+      landingPage.AssessmentpopupFields('TestAssessment' + iCount, 'TestOrganisation' + iCount, 'TestDomain' + iCount, 'TestIndustry', iCount, 'test@thoughtworks.com')
       landingPage.saveAssessmentButton().click()
     }
+  })
 
-    for (var iCount = 1; iCount <= 40; iCount++) {
+  it('Creating 30 assessments for testing purpose',()=> {
+    for (var iCount = 41; iCount <= 70; iCount++) {
       landingPage.clickCreateAssessment()
-      landingPage.AssessmentpopupFields('TestAssessment'+iCount,'TestOrganisation'+iCount,'TestDomain'+iCount,'TestIndustry',iCount,'test@thoughtworks.com')
+      landingPage.AssessmentpopupFields('TestAssessment' + iCount, 'TestOrganisation' + iCount, 'TestDomain' + iCount, 'TestIndustry', iCount, 'test@thoughtworks.com')
       landingPage.saveAssessmentButton().click()
     }
+  })
 
-    for (var iCount = 1; iCount <= 40; iCount++) {
+  it('Creating 30 assessments for testing purpose',()=> {
+    for (var iCount = 71; iCount <= 100; iCount++) {
       landingPage.clickCreateAssessment()
-      landingPage.AssessmentpopupFields('TestAssessment'+iCount,'TestOrganisation'+iCount,'TestDomain'+iCount,'TestIndustry',iCount,'test@thoughtworks.com')
+      landingPage.AssessmentpopupFields('TestAssessment' + iCount, 'TestOrganisation' + iCount, 'TestDomain' + iCount, 'TestIndustry', iCount, 'test@thoughtworks.com')
       landingPage.saveAssessmentButton().click()
     }
+  })
+
   })
   it('search prerequisites',()=>{
     landingPage.clickCreateAssessment()
