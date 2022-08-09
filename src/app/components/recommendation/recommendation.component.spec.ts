fb3b7c4b3fdaaf8cb713353d15ae792726599f1b
@brindha.e <brindha.e@thoughtworkworks.com>
2022-08-09 17:04:31 +0530

    [87][Brindha|Sahiti]Add. testcase for impact and effort change in recommendation component

--- src/app/components/recommendation/recommendation.component.spec.ts
+++ src/app/components/recommendation/recommendation.component.spec.ts
@@ -3,21 +3,20 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { RecommendationComponent } from './recommendation.component';
 import {MatFormFieldModule} from "@angular/material/form-field";
 import {NoopAnimationsModule} from "@angular/platform-browser/animations";
-import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {FormsModule, NgForm, ReactiveFormsModule} from "@angular/forms";
 import {MatInputModule} from "@angular/material/input";
 import {CommonModule} from "@angular/common";
 import {BrowserModule} from "@angular/platform-browser";
 import {MatSnackBarModule} from "@angular/material/snack-bar";
 import {MatCardModule} from "@angular/material/card";
 import {HttpClientTestingModule} from "@angular/common/http/testing";
-import {MatRadioChange, MatRadioModule} from "@angular/material/radio";
 import {StoreModule} from "@ngrx/store";
 import {reducers} from "../../reducers/reducers";
 import {AppServiceService} from "../../services/app-service/app-service.service";
 import {of, throwError} from "rxjs";
 import {TopicLevelRecommendationTextRequest} from "../../types/topicLevelRecommendationTextRequest";
 import {TopicLevelRecommendation} from "../../types/topicLevelRecommendation";
-
+import {MatTooltipModule} from "@angular/material/tooltip";
 
 class MockAppService {
   saveTopicRecommendationText(topicLevelRecommendationText: TopicLevelRecommendationTextRequest) {
@@ -34,38 +33,34 @@ class MockAppService {
       return throwError("Error!")
     }
   }
-
 }
 
 describe('RecommendationComponent', () => {
   let component: RecommendationComponent;
   let fixture: ComponentFixture<RecommendationComponent>;
-
-
   let mockAppService: MockAppService
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ RecommendationComponent ],
-      imports: [MatFormFieldModule, NoopAnimationsModule, FormsModule, ReactiveFormsModule, MatInputModule, CommonModule, BrowserModule, MatSnackBarModule, MatCardModule, HttpClientTestingModule,MatRadioModule,
+      declarations: [RecommendationComponent],
+      imports: [MatFormFieldModule, NoopAnimationsModule, FormsModule, ReactiveFormsModule, MatInputModule, CommonModule, BrowserModule, MatSnackBarModule, MatCardModule,MatTooltipModule, HttpClientTestingModule,
         StoreModule.forRoot(reducers)],
       providers: [{provide: AppServiceService,useClass: MockAppService}]
     })
       .compileComponents();
   });
 
-
   beforeEach(() => {
     mockAppService = new MockAppService()
     fixture = TestBed.createComponent(RecommendationComponent);
     component = fixture.componentInstance;
+   //fixture.detectChanges();
   });
-
   it('should create', () => {
     expect(component).toBeTruthy();
   });
 
-  it("should auto save parameter recommendation", async () => {
+  it("should auto save  recommendation", async () => {
     component.answerResponse1 = of({
       assessmentId: 5,
       assessmentName: "abc1",
@@ -115,7 +110,7 @@ describe('RecommendationComponent', () => {
   });
 
 
-  it("should auto save Delivery Horizon", async () => {
+  it("should auto save delivery horizon", async () => {
     component.answerResponse1 = of({
       assessmentId: 5,
       assessmentName: "abc1",
@@ -143,15 +138,15 @@ describe('RecommendationComponent', () => {
       parameterRatingAndRecommendation: [{parameterId: 1, rating: 2, recommendation: ""}]
     })
     let topicLevelRecommendationText = {
-      assessmentId: 0,topicId: 0, topicLevelRecommendation : {recommendationId : 1,deliveryHorizon:""}
+      assessmentId: 0,topicId: 0, topicLevelRecommendation : {recommendation : ""}
     };
     component.assessmentId = 1
     component.topicId = 1
     const keyEventData = {isTrusted: true, code: 'Key'};
     const keyEvent = new KeyboardEvent('keyup', keyEventData);
 
-    jest.spyOn(component, 'saveParticularRecommendationText')
-    component.recommendation = {recommendationId : 1, recommendation : "some more",impact:undefined ,effort : undefined ,deliveryHorizon :"new text"}
+    jest.spyOn(component, 'saveParticularRecommendationDeliveryHorizon')
+    component.recommendation = {recommendationId : undefined, recommendation : "some more",impact:undefined ,effort : undefined ,deliveryHorizon : "some text"}
     component.assessmentStatus="Active"
     component.ngOnInit()
     component.saveParticularRecommendationDeliveryHorizon(keyEvent);
@@ -161,7 +156,122 @@ describe('RecommendationComponent', () => {
     mockAppService.saveTopicRecommendationFields(topicLevelRecommendationText).subscribe(data => {
       expect(data).toBe(topicLevelRecommendationText)
     })
-    expect(component.topicRecommendationResponse.deliveryHorizon).toBe("new text");
+    expect(component.topicRecommendationResponse.deliveryHorizon).toBe("some text");
+  });
+
+
+  it("should auto save impact change", async () => {
+    let topicLevelRecommendationText : TopicLevelRecommendationTextRequest = {
+      assessmentId: 0,topicId: 0, topicLevelRecommendation : {recommendationId : 1 , recommendation:"text",impact:"HIGH", effort: "LOW", deliveryHorizon: "some text"}
+    };
+    component.assessmentId = 2
+    component.topicId = 2
+
+
+    jest.spyOn(component,'impactChange')
+    component.recommendation = {recommendationId :1, recommendation : "",impact:"LOW" ,effort : "" ,deliveryHorizon : ""}
+    component.ngOnInit()
+    component.impactChange();
+
+    await new Promise((r) => setTimeout(r, 2000));
+
+    mockAppService.saveTopicRecommendationFields(topicLevelRecommendationText).subscribe(data => {
+      expect(data).toBe(topicLevelRecommendationText)
+    })
+    expect(component.topicRecommendationResponse.impact).toBe("LOW");
+  });
+
+  it("should auto save effort change", async () => {
+    component.answerResponse1 = of({
+      assessmentId: 5,
+      assessmentName: "abc1",
+      organisationName: "Thoughtworks",
+      assessmentStatus: "Active",
+      updatedAt: 1654664982698,
+      domain: "",
+      industry: "",
+      teamSize: 0,
+      users: [],
+      answerResponseList: [
+        {
+          questionId: 1,
+          answer: "answer1"
+        }],
+      topicRatingAndRecommendation: [{topicId: 0, rating: 1, topicLevelRecommendation :[
+          {
+            recommendationId:1,
+            recommendation:"some text",
+            impact:"HIGH",
+            effect:"LOW",
+            deliveryHorizon:"some more text"
+          }
+        ]}],
+      parameterRatingAndRecommendation: [{parameterId: 1, rating: 2, recommendation: ""}]
+    })
+    component.assessmentId = 1
+    component.topicId = 1
+
+    let topicLevelRecommendationText : TopicLevelRecommendationTextRequest = {
+      assessmentId: 0,topicId: 0, topicLevelRecommendation : {recommendationId : 1 , recommendation:"text",impact:"HIGH", effort: "HIGH", deliveryHorizon: "some text"}
+    };
+
+
+    jest.spyOn(component,'effortChange')
+    component.recommendation = {recommendationId :1, recommendation : "",impact:"LOW" ,effort : "HIGH" ,deliveryHorizon : ""}
+    component.ngOnInit()
+    component.effortChange();
+
+    await new Promise((r) => setTimeout(r, 2000));
+
+    mockAppService.saveTopicRecommendationFields(topicLevelRecommendationText).subscribe(data => {
+      expect(data).toBe(topicLevelRecommendationText)
+    })
+    expect(component.topicRecommendationResponse.effort).toBe("HIGH");
+  });
+
+  it('should able to delete recommendation template',()=>{
+    let recommendation={
+      recommendationId:1,
+      recommendation:"some text",
+      impact:"HIGH",
+      effort:"LOW",
+      deliveryHorizon:"some dummy text"
+    }
+    let topicArray : TopicLevelRecommendation[];
+    topicArray =[];
+    component.topicRecommendationArray=topicArray;
+    component.topicRecommendationArray?.push(recommendation);
+
+    component.deleteTemplate(recommendation);
+
+    expect(component.topicRecommendationArray?.length).toBe(0);
+  })
+
+  it('should be able to enable the fields when recommendationId is defined',()=>{
+    let recommendationId : number | undefined;
+     let value : boolean;
+
+     recommendationId =1,
+     value = component.disableFields(recommendationId);
+
+     expect(value).toBe(false);
+  })
+
+  it('should be able to disable the fields when recommendationId is undefined',()=>{
+    let recommendationId : number | undefined;
+    let value : boolean;
+
+    recommendationId =undefined,
+      value = component.disableFields(recommendationId);
+
+    expect(value).toBe(true);
+  })
+
+  it("should call the error whenever a problem occurs", () => {
+    jest.spyOn(component, "showError")
+    component.showError("Error", "Close")
+    expect(component.showError).toHaveBeenCalled()
+
   });
 
   it("should throw error when problem occurs", async () => {
@@ -208,7 +318,7 @@ describe('RecommendationComponent', () => {
     await new Promise((r) => setTimeout(r, 2000));
 
 
-    mockAppService.saveTopicRecommendationText(topicLevelRecommendationText).subscribe((data) => {
+    mockAppService.saveTopicRecommendationFields(topicLevelRecommendationText).subscribe((data) => {
       expect(data).toBeUndefined()
     }, error => {
       expect(component.showError).toHaveBeenCalled()
@@ -216,48 +326,29 @@ describe('RecommendationComponent', () => {
     })
   });
 
-  it('should able to delete recommendation template',()=>{
-    let recommendation={
-      recommendationId:1,
-      recommendation:"some text",
-      impact:"HIGH",
-      effort:"LOW",
-      deliveryHorizon:"some dummy text"
+  it('should update topic level recommendation according to recommendation response', function () {
+    component.topicRecommendationResponse = {
+      assessmentId : 0,
+      topicId : 0,
+      recommendationId : 1,
+      recommendation : "text",
+      impact : "LOW",
+      effort : "HIGH",
+      deliveryHorizon : "text"}
+
+    component.topicRecommendationSample = [{
+      recommendationId : 1,
+      recommendation : "sample text",
+      impact : "LOW",
+      effort : "HIGH",
+      deliveryHorizon : "text"}];
+
+    component.getRecommendation(component.topicRecommendationSample,component.topicRecommendationResponse)
+
+    if (component.topicRecommendationSample) {
+      expect(component.topicRecommendationSample[0].recommendation).toBe("text");
     }
-    let topicArray : TopicLevelRecommendation[];
-    topicArray =[];
-    component.topicRecommendationArray=topicArray;
-    component.topicRecommendationArray?.push(recommendation);
 
-    component.deleteTemplate(recommendation);
-
-    expect(component.topicRecommendationArray?.length).toBe(0);
-  })
-
-  it('should be able to enable the fields when recommendationId is defined',()=>{
-    let recommendationId : number | undefined;
-     let value : boolean;
-
-     recommendationId =1,
-     value = component.disableFields(recommendationId);
-
-     expect(value).toBe(false);
-  })
-
-  it('should be able to disable the fields when recommendationId is undefined',()=>{
-    let recommendationId : number | undefined;
-    let value : boolean;
-
-    recommendationId =undefined,
-      value = component.disableFields(recommendationId);
-
-    expect(value).toBe(true);
-  })
-
-  it("should call the error whenever a problem occurs", () => {
-    jest.spyOn(component, "showError")
-    component.showError("Error", "Close")
-    expect(component.showError).toHaveBeenCalled()
   });
 
 });
