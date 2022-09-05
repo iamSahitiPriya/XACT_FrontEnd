57867a3fbe9d34b3bdbb38c7cfed404ad0bf3ded
JathinMeduri <102661843+JathinMeduri@users.noreply.github.com>
2022-09-05 13:27:36 +0530

    [Jathin] added/updated TC

--- cypress/pageObjects/loginPage.cy.js
+++ cypress/pageObjects/loginPage.cy.js
@@ -106,8 +106,10 @@ class loginPage {
     cy.clearCookies()
     cy.clearLocalStorage()
 
-    const username = Cypress.env('xAct_username')
-    const password = Cypress.env('xAct_password')
+    // const username = Cypress.env('xAct_username')
+    // const password = Cypress.env('xAct_password')
+    const username ='jathin.meduri@thoughtworks.com'
+    const password ='Newspaper@123'
     expect(username, 'username was set').to.be.a('string').and.not.be.empty
     if (typeof password !== 'string' || !password) {
       throw new Error('Missing password value, set using CYPRESS_password=...')
