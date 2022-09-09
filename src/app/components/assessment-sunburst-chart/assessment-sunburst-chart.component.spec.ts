52d84d709075447978c217524021b309c042b8ca
RUCHIKA MODGIL <ruchika.modgil@thoughtworks.com>
2022-09-09 23:24:46 +0530

    [125] [Ruchika] Create Component For SunBurst Chart

---
+++ src/app/components/assessment-sunburst-chart/assessment-sunburst-chart.component.spec.ts
@@ -0,0 +1,229 @@
+import {ComponentFixture, TestBed} from '@angular/core/testing';
+
+import {AssessmentSunburstChartComponent} from './assessment-sunburst-chart.component';
+import {HttpClientTestingModule} from "@angular/common/http/testing";
+import {RouterTestingModule} from "@angular/router/testing";
+import {StoreModule} from "@ngrx/store";
+import {reducers} from "../../reducers/reducers";
+import * as d3 from 'd3';
+
+
+interface MockSvgTextElement {
+  prototype: {
+    getBBox: () => { height: number; width: number };
+  };
+}
+
+describe('AssessmentSunburstChartComponent', () => {
+  let component: AssessmentSunburstChartComponent;
+  let fixture: ComponentFixture<AssessmentSunburstChartComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ AssessmentSunburstChartComponent ],
+      imports: [ HttpClientTestingModule , RouterTestingModule, StoreModule.forRoot(reducers)]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AssessmentSunburstChartComponent);
+    component = fixture.componentInstance;
+
+
+  });
+  afterEach(function() {
+    d3.selectAll('svg').remove();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should call the function', function() {
+    component.data =  { name:"project",children:[{
+            name:"ass1",rating:3,children:[{
+              name:"ass2",rating:2,children:[{
+                name:"ass3",rating:3,children:[{
+                  name:"ass3",value:1
+                  }]
+                }]
+              }]
+            }]}
+    jest.spyOn(component, 'getSunBurstChart');
+    Object.defineProperty(global.SVGElement.prototype, 'getComputedTextLength', {
+      writable: true,
+      value: jest.fn().mockReturnValue(100),
+    });
+    component.getSunBurstChart(component.data);
+    console.log(fixture.nativeElement.querySelector("svg").getAttribute("width"));
+  })
+  it('should change theme of chart onClick', () =>{
+    jest.spyOn(component, 'onClick');
+
+    component.data =  { name:"project",children:[{
+        name:"ass1",rating:3,children:[{
+          name:"ass2",rating:2,children:[{
+            name:"ass3",rating:3,children:[{
+              name:"ass3",value:1
+            }]
+          }]
+        }]
+      }]}
+    component.selectedValue = d3.interpolatePurples;
+    component.onClick(d3.interpolateSpectral)
+    expect(component.selectedValue).toBe(d3.interpolateSpectral)
+
+  })
+  it('should change theme to threat display' ,() => {
+    jest.spyOn(component, 'onClick');
+
+    component.data =  { name:"project",children:[{
+        name:"ass1",rating:3,children:[{
+          name:"ass2",rating:2,children:[{
+            name:"ass3",rating:3,children:[{
+              name:"ass3",value:1
+            }]
+          }]
+        }]
+      }]}
+    component.selectedValue = d3.interpolatePurples;
+    component.onClick(null)
+    expect(component.selectedValue).toBe(null);
+
+  })
+  it('should initialize breadCrumb', () => {
+    let graphContainer = document.createElement("div");
+    graphContainer.id = "sequence";
+    document.body.appendChild(graphContainer);
+    // d3.select(graphContainer.id).selectAll("svg").remove();
+    jest.spyOn(component,'initializeBreadcrumbTrail');
+    component.initializeBreadcrumbTrail(graphContainer.id)
+    console.log(d3.select("#sequence").select("svg").attr("width"))
+
+  })
+
+  it('should get Ancestors of data selected', () => {
+    jest.spyOn(component, 'getAncestors');
+
+    component.data =  { name:"project",children:[{
+        name:"ass1",rating:3,children:[{
+          name:"ass2",rating:2,children:[{
+            name:"ass3",rating:3,children:[{
+              name:"ass3",value:1
+            }]
+          }]
+        }]
+      }]}
+    component.getAncestors(component.data.children[0])
+    expect(component.getAncestors(component.data.children[0])).not.toBeNull();
+  })
+  it('should be able to create figure of breadCrumbs', () => {
+    jest.spyOn(component, 'breadcrumbFigure');
+
+    component.data =  { name:"project",children:[{
+        name:"ass1",rating:3,children:[{
+          name:"ass2",rating:2,children:[{
+            name:"ass3",rating:3,children:[{
+              name:"ass3",value:1
+            }]
+          }]
+        }]
+      }]}
+    var actualFigurePoints = component.breadcrumbFigure(component.data.children[0],2);
+    var expectedFigurePoints = "0,0 155,26.666666666666668 310,0 310,80 155,120 0,80";
+    expect(actualFigurePoints).toBe(expectedFigurePoints)
+
+  })
+  it("should update breadCrumbs", () => {
+    jest.spyOn(component,'updateBreadcrumbs');
+    let mockedNodeArray = [{
+      name:"ass1",rating:3,children:[{
+        name:"ass2",rating:2,children:[{
+          name:"ass3",rating:3,children:[{
+            name:"ass3",value:1
+          }]
+        }]
+      }]
+    }]
+    component.updateBreadcrumbs(mockedNodeArray,2)
+  })
+  it("should fill color as green if average rating is more than 3", () => {
+    jest.spyOn(component, 'fillThreatColorsInChart');
+
+    let node = {
+      data:{
+        name:"ass1",rating:4
+      }
+    }
+    expect(component.fillThreatColorsInChart(node)).toBe("green");
+  })
+  it("should fill color as red if average rating is less than 3", () => {
+    jest.spyOn(component, 'fillThreatColorsInChart');
+
+    let node = {
+      data:{
+        name:"ass1",rating:1
+      }
+    }
+    expect(component.fillThreatColorsInChart(node)).toBe("red");
+  })
+
+  it("should fill color as orange if average rating is equal to 3", () => {
+    jest.spyOn(component, 'fillThreatColorsInChart');
+
+    let node = {
+      data:{
+        name:"ass1",rating:3
+      }
+    }
+    expect(component.fillThreatColorsInChart(node)).toBe("orange");
+  })
+
+  it("should fill rating color as green if rating is more than 3", () => {
+    jest.spyOn(component, 'fillRatingCircle');
+
+    let rating =4;
+    expect(component.fillRatingCircle(rating)).toBe("green");
+  })
+
+  it("should fill rating color as red if average rating is less than 3", () => {
+    jest.spyOn(component, 'fillRatingCircle');
+
+    let rating = 2
+    expect(component.fillRatingCircle(rating)).toBe("red");
+  })
+
+  it("should fill rating color as orange if  rating is equal to 3", () => {
+    jest.spyOn(component, 'fillRatingCircle');
+
+    let rating = 3
+    expect(component.fillRatingCircle(rating)).toBe("orange");
+  })
+  it("should return name of particular element of data from structure", () => {
+    jest.spyOn(component, 'getDataName');
+    let dummyData= {data:{
+      name:"category1",rating:3
+      }}
+    expect(component.getDataName(dummyData)).toBe("category1")
+  })
+  it("should check MouseLeave Functionality", () => {
+    jest.spyOn(component,'onMouseleave');
+    let anyData;
+    component.onMouseleave(anyData);
+    expect(d3.select("#trail")).not.toBeNull();
+  })
+
+  it("should be able to change position of upcoming breadCrumbs" , () => {
+    jest.spyOn(component,'getBreadCrumbTranslation');
+    let dummyData1 = {
+      name:"category", depth:2
+    }
+    expect(component.getBreadCrumbTranslation(dummyData1)).toBe("translate(0,210)")
+    let dummyData2 = {
+      name:"category", depth:3
+    }
+    expect(component.getBreadCrumbTranslation(dummyData2)).toBe("translate(0,315)")
+  })
+
+});
---
+++ src/app/components/assessment-sunburst-chart/assessment-sunburst-chart.component.ts
@@ -0,0 +1,401 @@
+import {Component, OnInit} from '@angular/core';
+import * as d3 from 'd3';
+import {AppServiceService} from "../../services/app-service/app-service.service";
+import {ActivatedRoute} from "@angular/router";
+import * as fromActions from "../../actions/assessment-data.actions";
+import {Store} from "@ngrx/store";
+import {AssessmentState} from "../../reducers/app.states";
+import {ReportDataStructure} from "../../types/ReportDataStructure";
+import {CategoryStructure} from "../../types/categoryStructure";
+import {BehaviorSubject} from "rxjs";
+import {selectAll} from "d3";
+
+interface colorScheme{
+  value?:(t:number) => string,
+  viewValue:string
+}
+
+@Component({
+  selector: 'app-assessment-sunburst-chart',
+  templateUrl: './assessment-sunburst-chart.component.html',
+  styleUrls: ['./assessment-sunburst-chart.component.css']
+})
+
+export class AssessmentSunburstChartComponent implements OnInit {
+  assessmentId: number;
+  data:ReportDataStructure;
+  selectedValue: (t: number) => string;
+
+
+  colorList: colorScheme[] = [{value: d3.interpolateRainbow,viewValue:'Rainbow Theme'},
+    {value:d3.interpolateReds,viewValue:'All Red'},
+    {value:d3.interpolateRdPu,viewValue:'Purple Red'},
+    {value:d3.interpolatePurples,viewValue:'All Purple'},
+    {value:d3.interpolateWarm, viewValue:'Warm Theme'},
+    {value:d3.interpolateBlues,viewValue:'All Blue'},
+    {value:d3.interpolateSpectral,viewValue:'Spectral Colors'},
+    {viewValue:'Show Threats'}
+  ];
+
+  ngOnInit() {
+    const assessmentIdParam = this.route.snapshot.paramMap.get('assessmentId') || 0;
+    this.assessmentId = +assessmentIdParam;
+    this.store.dispatch(fromActions.getAssessmentId({id: this.assessmentId}))
+    this.getData();
+  }
+  constructor(private appService: AppServiceService, private route: ActivatedRoute, private store: Store<AssessmentState>) {
+  }
+
+  getData() {
+    this.appService.getReportData(this.assessmentId).subscribe(data => {
+      this.data =  data;
+      this.getSunBurstChart(this.data)
+    })
+  }
+
+  getSunBurstChart(data:ReportDataStructure) {
+
+      let partition = (data: any) => {
+        const root = d3.hierarchy(data)
+          .sum(d => d.value)
+          .sort((a: any, b: any) => b.value - a.value);
+        return d3.partition()
+          .size([2 * Math.PI, root.height + 1])
+          (root);
+      }
+
+      let color = d3.scaleOrdinal(d3.quantize(d3.interpolatePurples, data.children.length + 7).reverse());
+
+      let width = 800;
+      let breadCrumbId = document.getElementById("sequence")
+      this.initializeBreadcrumbTrail(breadCrumbId)
+
+      let radius = width / 10.5
+
+      let arc = d3.arc()
+        .startAngle((d: any) =>
+        {console.log(d);
+          return d.x0})
+        .endAngle((d: any) => d.x1)
+        .padAngle((d: any) => Math.min((d.x1 - d.x0) / 2, 0.005))
+        .padRadius(radius * 1.5)
+        .innerRadius((d: any) => d.y0 * radius)
+        .outerRadius((d: any) => Math.max(d.y0 * radius, d.y1 * radius - 1))
+
+
+      const root = partition(this.data);
+
+      root.each((d: any) => d.current = d);
+
+      const svg = d3.select("#chart")
+        .attr("width",width)
+        .append("svg")
+        .attr("width", "100%")
+        .attr("height", "100%")
+        .attr('viewBox','0 0 '+580+' '+1000)
+        .style("font", "10px sans-serif")
+        .classed("svg-content-responsive", true);
+
+
+    const vis = svg.append("svg:g")
+        .attr("id", "container")
+        .attr("transform", `translate(290,460)`);
+
+      d3.select("#container").on("mouseleave",this.onMouseleave);
+
+
+      const path = vis.append("g")
+        .selectAll("path")
+        .data(root.descendants().slice(1))
+        .enter().append("path")
+        .attr("fill", (d: any) => {
+          while (d.depth > 1) d = d.parent;
+          return color(d.data.name);
+        })
+        .attr("fill-opacity", (d: any) => arcVisible(d.current) ? (((d.data.rating<3 && d.data.rating>0) || d.data.value<3)? 0.9 : 0.7) : 0)
+        .attr("d", (d: any) => {
+          return <any>arc(d.current);
+        })
+        .on("mouseover", (event: any, d: any) => {
+      let percentageText = 0;
+      if (!d.data.rating || d.value == 0 ) {
+        percentageText = d.value;
+      } else {
+        percentageText = d.data.rating;
+      }
+      var sequenceArray = this.getAncestors(d);
+
+      this.updateBreadcrumbs(sequenceArray,percentageText);
+          d3.selectAll("path")
+        .style("opacity", 0.3);
+
+          d3.select("#chart").select("#container").selectAll("path")
+        .filter(function (node: any) {
+          return (sequenceArray.indexOf(node) >= 0);
+        })
+        .style("opacity", 1);
+    })
+
+      path.filter((d: any) => d.children)
+        .style("cursor", "pointer")
+        .on("click", clicked);
+
+      path.append("title")
+        .text((d: any) => d.data.name)
+        .style("font","21px");
+
+
+      const label = vis.append("g")
+        .attr("pointer-events", "none")
+        .attr("text-anchor", "middle")
+        .style("user-select", "none")
+        .selectAll("text")
+        .data(root.descendants().slice(1))
+        .enter().append("text")
+        .attr("x",0)
+        .attr("dy", "-0.07px")
+        .attr("fill-opacity", (d: any) => +labelVisible(d.current))
+        .attr("transform", (d: any) => labelTransform(d.current))
+        .style("overflow-y","auto")
+        .text((d: any) => d.data.name)
+        .style("font", "7.3px Inter")
+    .call(this.wrap, 75);
+      ;
+
+
+      const parent = vis.append("circle")
+        .datum(root)
+        .attr("r", radius)
+        .attr("fill", "none")
+        .attr("pointer-events", "all")
+        .on("click", clicked)
+
+      function clicked(event: any, p: any) {
+        d3.select("#chart").select("#container").select("circle").datum(p.parent || root);
+
+        root.each((d: any) => d.target = {
+          x0: Math.max(0, Math.min(1, (d.x0 - p.x0) / (p.x1 - p.x0))) * 2 * Math.PI,
+          x1: Math.max(0, Math.min(1, (d.x1 - p.x0) / (p.x1 - p.x0))) * 2 * Math.PI,
+          y0: Math.max(0, d.y0 - p.depth),
+          y1: Math.max(0, d.y1 - p.depth)
+        });
+
+        const t =  d3.select("#chart").select("#container").transition().duration(750);
+        path.transition(<any>t)
+          .tween("data", (d: any) => {
+            const i = d3.interpolate(d.current, d.target);
+            return (t: any) => d.current = i(t);
+          })
+          .filter(function (d: any) {
+            return +<any>this.getAttribute("fill-opacity") || <any>arcVisible(d.target);
+          })
+          .attr("pointer-events", (d: any) => arcVisible(d.target) ? "auto" : "none")
+          .attrTween("d", (d: any) => () => <any>arc(d.current));
+
+        label.filter(function (d: any) {
+          return +<any>this.getAttribute("fill-opacity") || <any>labelVisible(d.target)
+        }).transition(<any>t)
+          .attr("fill-opacity", (d: any) => +labelVisible(d.target))
+          .attrTween("transform", (d: any) => () => labelTransform(d.current));
+      }
+
+      function arcVisible(d: any) {
+        return d.y1 <= 5 && d.y0 >= 1 && d.x1 > d.x0;
+      }
+
+      function labelVisible(d: any) {
+        return d.y1 <= 5 && d.y0 >= 1 && (d.y1 - d.y0) * (d.x1 - d.x0) > 0.03;
+      }
+
+      function labelTransform(d: any) {
+        const x = (d.x0 + d.x1) / 2 * 180 / Math.PI;
+        const y = (d.y0 + d.y1) / 2 * radius;
+        return `rotate(${x - 90}) translate(${y},0) rotate(${x < 180 ? 0 : 180})`;
+      }
+
+    return svg.node();
+
+  }
+
+ onMouseleave(d:any){
+   d3.select("#trail")
+     .style("visibility", "hidden");
+   d3.selectAll("path")
+     .style("opacity", 1)
+ }
+
+
+   getAncestors(node:any) {
+    var path = [];
+    var current = node;
+    while (current) {
+      path.unshift(current);
+      current = current.parent;
+    }
+    return path;
+  }
+
+   initializeBreadcrumbTrail(id:any) {
+     var trail = d3.select("#sequence")
+      .append("svg")
+      .attr("width", 320)
+      .attr("height", 850)
+      .attr("id", "trail")
+      .attr("fill","purple")
+      .attr("fill-opacity",0.6);
+
+    trail.append("svg:circle")
+      .attr("id", "endlabel")
+     trail.append("svg:text")
+      .attr("id","ratingText")
+  }
+
+   breadcrumbFigure(d:any, i:any) {
+    let breadCrumbPoints = {
+        s:20,t:10,d:105,w:310,h:80
+    };
+    var points = [];
+    points.push("0,0");
+    if (i > 0) {
+      points.push((breadCrumbPoints.w/2) + "," + breadCrumbPoints.h/3);
+    }
+    points.push(breadCrumbPoints.w + ",0");
+    points.push(breadCrumbPoints.w+ "," + breadCrumbPoints.h);
+    points.push(breadCrumbPoints.w/2+","+ (<number>breadCrumbPoints.h+40));
+    points.push("0," + breadCrumbPoints.h);
+
+    return points.join(" ");
+  }
+
+   updateBreadcrumbs = (nodeArray: any, percentageString: any) => {
+    let breadCrumbPoints = {
+      s:20,t:10,d:105,w:310,h:80
+    };
+    var g = d3.select("#trail")
+      .selectAll("g")
+      .data(nodeArray, function(d:any) { return d.data.name + d.depth; })
+
+    var entering = g.enter().append("svg:g")
+      .attr("transform", this.getBreadCrumbTranslation)
+
+    entering.append("polygon")
+      .attr("points", this.breadcrumbFigure)
+
+     entering.append("svg:text")
+       .attr("x", (breadCrumbPoints.w + breadCrumbPoints.t) / 2)
+       .attr("y", breadCrumbPoints.h/1.8)
+       .attr("dy", "1.5em")
+       .attr("text-anchor", "middle")
+       .attr("fill","black")
+       .attr("fill-opacity",1)
+       .text(this.getDataName)
+       .style("font", "14px Inter")
+       .call(this.wrap,300);
+
+     g.exit().remove();
+
+
+    d3.select("#trail").select("#endlabel")
+        .attr("r",25)
+        .attr("fill",this.fillRatingCircle(percentageString))
+        .attr("cx", (breadCrumbPoints.w/2)+45)
+        .attr("cy", (nodeArray.length + 0.3) * (breadCrumbPoints.h + breadCrumbPoints.s))
+
+
+      d3.select("#trail").select("#ratingText")
+      .attr("x", (breadCrumbPoints.w/2)+45)
+      .attr("y", (nodeArray.length + 0.3) * (breadCrumbPoints.h + breadCrumbPoints.s))
+      .attr("fill","white")
+      .attr("fill-opacity",1)
+      .style("font", "14px Inter")
+      .text(percentageString);
+
+    d3.select("#trail")
+      .style("visibility", "");
+
+  }
+
+  getDataName(d:any){
+    return d.data.name
+  }
+
+  getBreadCrumbTranslation(d:any) {
+    let breadCrumbPoints = {
+      s:20,t:10,d:105,w:310,h:80
+    };
+    return "translate(" + 0 + ","+ d.depth * (breadCrumbPoints.d) +")";
+  }
+
+   wrap(text:any, width:any) {
+
+    text.each(function(this:any) {
+      var text = d3.select(<any>this),
+        words = text.text().split(/\s+/).reverse(),
+        word,
+        line:any = [],
+        lineNumber = 0,
+        lineHeight = 0.05, // ems
+        y = text.attr("y"),
+        x = text.attr("x"),
+        dy = parseFloat(text.attr("dy")),
+        tspan = text.text(null).append("tspan").attr("x", x).attr("y", y).attr("dy", dy + "em") ;
+
+      while (word = words.pop()) {
+        line.push(word);
+        tspan.text(line.join(" "));
+        var len = tspan.node()?.getComputedTextLength();
+        if (<any>len > width) {
+          line.pop();
+          tspan.text(line.join(" "));
+          line = [word];
+          tspan = text.append("tspan").attr("x", x).attr("y", y).attr("dy", lineNumber++ * lineHeight + dy + 0.85+ "em").text(word);
+        }
+      }
+    });
+  }
+
+
+  onClick(val:any) {
+    this.selectedValue = val
+    if (this.selectedValue == null) {
+      d3.select("#trail")
+        .attr("fill", "orange")
+
+      d3.selectAll("path")
+        .attr("fill", this.fillThreatColorsInChart)
+    } else {
+      let color = d3.scaleOrdinal(d3.quantize(this.selectedValue, this.data.children.length + 3).reverse());
+      let breadCrumbColor = <string>color("1")
+      d3.select("#trail")
+        .attr("fill", breadCrumbColor)
+      d3.selectAll("path")
+        .attr("fill", (d: any) => {
+          while (d.depth > 1) d = d.parent;
+          return <string>color(d.data.name);
+        })
+    }
+  }
+
+  fillThreatColorsInChart(d:any){
+    if ((d.data.rating < 3 && d.data.rating>0) || d.data.value <3) {
+      return "red"
+    } else if (d.data.rating == 3 || d.data.value == 3) {
+      return "orange"
+    } else {
+      return "green"
+    }
+  }
+
+  fillRatingCircle(percentageString:any){
+    if(percentageString>3){
+      return "green"
+    }
+    else if(percentageString ==3 ){
+      return "orange"
+    }
+    else {
+      return "red"
+    }
+  }
+
+}
