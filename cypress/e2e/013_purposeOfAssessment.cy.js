73532460ca467d560604aa1546ec8ff19b613347
JathinMeduri <102661843+JathinMeduri@users.noreply.github.com>
2022-10-17 21:52:52 +0530

    Added/Updated TC

---
+++ cypress/e2e/013_purposeOfAssessment.cy.js
@@ -0,0 +1,57 @@
+import landingPage from "../pageObjects/landingPage.cy";
+import loginPage from "../pageObjects/loginPage.cy";
+import commonFunctions from "../pageObjects/commonFunction.cy";
+import assessmentPage from "../pageObjects/assessmentPage.cy";
+describe('validating functionality of login page of xAct application', () => {
+
+  beforeEach('User should get navigated to Okta by launching the url', () => {
+    cy.window().then(win => win.location.hash = "/foo/bar")
+    cy.intercept('GET', 'https://dev-47045452.okta.com/oauth2/default/v1/userinfo').as('pageLoad')
+    cy.visit('/')
+  })
+
+  it('tc001 Verifying whether purpose of assessment dropdown is displayed', () => {
+    landingPage.clickCreateAssessmentButton()
+    commonFunctions.elementIsVisible(landingPage.AssessmentPopup(),'Assessment popup is visible')
+    commonFunctions.elementIsVisible(landingPage.purposeOfAssessment(),'purpose of Assessment dropdown is visible')
+  })
+
+  it('tc002 Verifying whether user can select available options in the purpose of assessment dropdown', () => {
+    landingPage.clickCreateAssessmentButton()
+    commonFunctions.elementIsVisible(landingPage.AssessmentPopup(),'Assessment popup is visible')
+    commonFunctions.elementIsVisible(landingPage.purposeOfAssessment(),'purpose of Assessment dropdown is visible')
+    commonFunctions.clickOnElement(landingPage.purposeOfAssessment().click(),'purpose of element is visible')
+    commonFunctions.clickOnElement(landingPage.purposeOfAssessmentOption(1),'Client Request option is clicked')
+    commonFunctions.containsText(landingPage.purposeOfAssessment(),'','Client request option is selected in purpose of assessment dropdown')
+    commonFunctions.clickOnElement(landingPage.purposeOfAssessmentOption(2),'Internal Request option is clicked')
+    commonFunctions.containsText(landingPage.purposeOfAssessment(),'Internal Request','Internal request option is selected in purpose of assessment dropdown')
+  })
+
+  it('tc003 Verifying whether purpose of assessment dropdown is displayed in configure assessment popup', () => {
+    commonFunctions.clickOnElement(landingPage.optionsButton(),'Quick link option is clicked')
+    commonFunctions.clickOnElement(landingPage.configureOption(),'configure assessment option is clicked')
+    commonFunctions.elementIsVisible(landingPage.AssessmentPopup(),'configure Assessment popup is visible')
+    commonFunctions.elementIsVisible(landingPage.purposeOfAssessment(),'purpose of Assessment dropdown is visible')
+  })
+
+
+  it('tc004 Validating whether Manage option is renamed as configure in quick link', () => {
+    commonFunctions.clickOnElement(landingPage.optionsButton(),'Quick link option is clicked')
+    commonFunctions.clickOnElement(landingPage.configureOption(),'configure assessment option is clicked')
+    commonFunctions.elementIsVisible(landingPage.AssessmentPopup(),'configure Assessment popup is visible')
+    commonFunctions.elementIsVisible(landingPage.purposeOfAssessment(),'purpose of Assessment dropdown is visible')
+    commonFunctions.clickOnElement(landingPage.purposeOfAssessment2(),'purpose of element dropdown options are visible')
+    commonFunctions.clickOnElement(landingPage.purposeOfAssessmentOption(1),'Client Request option is clicked')
+    commonFunctions.containsText(landingPage.purposeOfAssessment(),'','Client request option is selected in purpose of assessment dropdown')
+    commonFunctions.clickOnElement(landingPage.purposeOfAssessmentOption(2),'Internal Request option is clicked')
+    commonFunctions.containsText(landingPage.purposeOfAssessment(),'Internal Request','Internal request option is selected in purpose of assessment dropdown')
+  })
+
+  it('tc005 Validating placeholder of purpose of assessment dropdown', () => {
+    landingPage.clickCreateAssessment()
+    commonFunctions.containsText(landingPage.purposeOfAssessment2(),'Purpose Of Assessment','Purpose of assessment placeholder is verified')
+  })
+
+
+
+})
