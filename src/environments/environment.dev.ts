33ff02f71d6be5856b3cc179eb73c4f593c77a7d
rampg23 <ramprakash.pg@thoughtworks.com>
2022-10-26 11:12:58 +0530

    [157][Ram|Brindha] Add Account URI to all envs.

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -35,6 +35,7 @@ export const environment = {
   UPDATE_CATEGORY_URI : "/v1/admin/categories",
   REPORT_DATA_URI:"/v1/reports/sunburst",
   REPORT_TEMPLATE_URI: "/v1/reports/template",
+  ACCOUNT_URI : "/v1/accounts",
   okta: {
     cookies: {
       secured: true
