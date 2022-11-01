877994557a7e0b1ee44628138583922e0aa44595
SahitiPriyaA <sahitipriya.a@thoughtworks.com>
2022-11-01 13:55:09 +0530

    [106][Sahiti | Ishika]Create admin module component

---
+++ src/app/components/admin/admin-module/admin-module.component.ts
@@ -0,0 +1,10 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-module',
+  templateUrl: './admin-module.component.html',
+  styleUrls: ['./admin-module.component.css']
+})
+export class AdminModuleComponent {
+
+}
