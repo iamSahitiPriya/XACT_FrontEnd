5266b447e7a6df7c6e0c0720d11795aca0ebbcfe
SahitiPriyaA <sahitipriya.a@thoughtworks.com>
2022-11-03 16:37:42 +0530

    [106]|[Sahiti]Add functionality to add row and styles for table

--- src/app/components/admin/admin-module/admin-module.component.ts
+++ src/app/components/admin/admin-module/admin-module.component.ts
@@ -1,10 +1,110 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit, ViewChild} from '@angular/core';
+import {ModuleData} from "../../../types/moduleData";
+import {MatTable, MatTableDataSource} from "@angular/material/table";
+import {Subject, takeUntil} from "rxjs";
+import {CategoryData} from "../../../types/category";
+import {AppServiceService} from "../../../services/app-service/app-service.service";
+import {MatSnackBar} from "@angular/material/snack-bar";
+import {data_local} from "../../../messages";
+import {MatPaginator} from "@angular/material/paginator";
+import {animate, state, style, transition, trigger} from "@angular/animations";
 
 @Component({
   selector: 'app-admin-module',
   templateUrl: './admin-module.component.html',
-  styleUrls: ['./admin-module.component.css']
+  styleUrls: ['./admin-module.component.css'],
+  animations: [
+    trigger('detailExpand', [
+      state('collapsed', style({height: '0px', minHeight: '0'})),
+      state('expanded', style({height: '100px'})),
+      transition('expanded <=> collapsed', animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')),
+    ]),
+  ],
 })
-export class AdminModuleComponent {
+export class AdminModuleComponent implements OnInit, OnDestroy{
+  moduleStructure : ModuleData[];
+  displayedColumns: string[] = ['categoryName','moduleName','updatedAt','active','edit'];
+  displayColumns: string[] = [...this.displayedColumns, 'expand'];
+  dataSource : MatTableDataSource<ModuleData>
+  commonErrorFieldText = data_local.ASSESSMENT.ERROR_MESSAGE_TEXT;
+  isModuleAdded: boolean = false;
 
+  private destroy$: Subject<void> = new Subject<void>();
+
+  @ViewChild(MatTable) table: MatTable<ModuleData>
+  @ViewChild(MatPaginator, {static:true}) paginator: MatPaginator;
+
+  dataSourceArray : ModuleData[];
+  dataToDisplayed :ModuleData[];
+  selectedModule: ModuleData | null;
+
+  constructor(private appService: AppServiceService, private _snackbar:MatSnackBar) {
+    this.moduleStructure = []
+    this.dataSource = new MatTableDataSource<ModuleData>(this.moduleStructure)
+    this.dataToDisplayed = [...this.dataSource.data]
+
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
+
+  ngOnInit(): void {
+    this.appService.getAllCategories().pipe(takeUntil(this.destroy$)).subscribe(data => {
+      data.forEach(eachCategory =>
+        eachCategory.modules.forEach(eachModule => {
+          let module: ModuleData = {
+            moduleId: -1,
+            moduleName: "",
+            categoryName: "",
+            active: true,
+            updatedAt: -1,
+            comments: ""
+          }
+          module.moduleId = eachModule.moduleId;
+          module.moduleName = eachModule.moduleName;
+          module.active = eachModule.active;
+          module.categoryName = eachCategory.categoryName;
+          module.updatedAt = eachModule.updatedAt;
+          module.comments = eachModule.comments;
+          this.moduleStructure.push(module);
+        })
+      )
+      this.dataSource = new MatTableDataSource<ModuleData>(this.moduleStructure)
+      this.dataSourceArray = [...this.dataSource.data]
+      this.paginator.pageIndex = 0
+      this.dataSource.paginator = this.paginator;
+    })
+  }
+
+  addModuleRow() {
+    let newModule = {
+      moduleId: 0, categoryName: '',moduleName: '', active: true, updatedAt: Date.now(), isEdit: true, comments: ''
+    }
+    this.dataSource.data.splice(this.paginator.pageIndex * this.paginator.pageSize, 0, newModule)
+    this.table.renderRows();
+    this.dataSource.paginator = this.paginator
+    this.isModuleAdded = true
+  }
+
+  updateCategory(row :any) {
+
+  }
+
+  cancelChanges(row : any) {
+
+  }
+
+  editCategory(row :any) {
+
+  }
+
+  deleteRow() {
+
+  }
+
+  saveCategory(row :any) {
+
+  }
 }
