ad83d7d860186f1a213097d20b4f3ea07e97d31e
SahitiPriyaA <sahitipriya.a@thoughtworks.com>
2022-11-14 17:39:19 +0530

    [111] | [Sahiti|Maitri] Create table for parameter details

---
+++ src/app/types/ParameterData.ts
@@ -0,0 +1,13 @@
+export interface ParameterData{
+  categoryId :number,
+  categoryName:string,
+  moduleId:number,
+  moduleName :string,
+  topicId:number,
+  topicName:string,
+  parameterId:number,
+  parameterName:string,
+  active:boolean,
+  updatedAt : number,
+  comments ?: string
+}
