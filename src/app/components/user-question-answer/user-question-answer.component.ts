f234d1a33a9ba7424bed23f5924666f705414f81
RUCHIKA MODGIL <ruchika.modgil@thoughtworks.com>
2022-11-24 17:46:35 +0530

    [180] [Ruchika] Create Component for UserQuestionAndAnswer

---
+++ src/app/components/user-question-answer/user-question-answer.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-user-question-answer',
+  templateUrl: './user-question-answer.component.html',
+  styleUrls: ['./user-question-answer.component.css']
+})
+export class UserQuestionAnswerComponent {
+
+  constructor() { }
+
+}
