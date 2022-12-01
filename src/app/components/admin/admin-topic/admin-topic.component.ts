7f5f6abfee80612cdcdff635a07c2ab068ca3498
SahitiPriyaA <sahitipriya.a@thoughtworks.com>
2022-12-01 16:09:49 +0530

    [111]|[Sahiti | Ram] Add mat sort header for columns

--- src/app/components/admin/admin-topic/admin-topic.component.ts
+++ src/app/components/admin/admin-topic/admin-topic.component.ts
@@ -15,11 +15,11 @@ import {animate, state, style, transition, trigger} from "@angular/animations";
 import {NotificationSnackbarComponent} from "../../notification-component/notification-component.component";
 import {CategoryResponse} from "../../../types/categoryResponse";
 import {ModuleStructure} from "../../../types/moduleStructure";
-import cloneDeep from "lodash/cloneDeep";
 import * as fromActions from "../../../actions/assessment-data.actions";
 
 import {Store} from "@ngrx/store";
 import {AppStates} from "../../../reducers/app.states";
+import {cloneDeep} from "lodash";
 
 @Component({
   selector: 'app-admin-topic',
@@ -38,7 +38,7 @@ export class AdminTopicComponent implements OnInit, OnDestroy {
   categories: CategoryResponse[]
   masterData: Observable<CategoryResponse[]>
   topicData: TopicData[];
-  displayedColumns: string[] = ['categoryName', 'moduleName', 'topicName', 'active', 'updatedAt', 'edit', 'reference'];
+  displayedColumns: string[] = ['categoryName', 'moduleName', 'topicName', 'updatedAt', 'active','edit', 'reference'];
   commonErrorFieldText = data_local.ADMIN.ERROR;
   dataSource: MatTableDataSource<TopicData>;
   displayColumns: string[] = [...this.displayedColumns, 'expand'];
@@ -251,7 +251,7 @@ export class AdminTopicComponent implements OnInit, OnDestroy {
     row.isEdit = false
     let categoryId = this.categoryList.find(category => category.categoryName == row.categoryName).categoryId
     this.moduleList = this.categoryAndModule.get(categoryId)
-    this.unsavedTopic = cloneDeep(row)
+    this.unsavedTopic =cloneDeep(row);
     return this.selectedTopic
   }
 
