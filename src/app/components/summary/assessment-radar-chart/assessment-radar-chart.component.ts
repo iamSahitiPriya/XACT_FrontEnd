2afb7f808a2e74826aea5456b4d31cd6aec8510b
rampg23 <ramprakash.pg@thoughtworks.com>
2023-02-08 18:03:02 +0530

    [289][Ram|Sahiti] Refactor styling for spider chart

--- src/app/components/summary/assessment-radar-chart/assessment-radar-chart.component.ts
+++ src/app/components/summary/assessment-radar-chart/assessment-radar-chart.component.ts
@@ -5,7 +5,9 @@ import {ChartConfiguration, ChartData, ChartType} from 'chart.js';
 interface RadarDataset {
   data: number[],
   fill: boolean,
-  label: string
+  label: string,
+  borderColor:string,
+  borderWidth:number
 }
 
 interface RadarChart {
@@ -59,13 +61,12 @@ export class AssessmentRadarChartComponent implements OnInit {
           eachCategory?.children?.forEach(eachModule => {
             if (eachModule.rating !== 0) {
               let radarChartData: RadarChartData = {moduleName: eachModule.name, data: {labels: [], datasets: []}}
-              let dataset: RadarDataset = {data: [], fill: false, label: "Current Score"}
-              let targetDataset: RadarDataset = {data: [], fill: false, label: "Target Score"}
+              let dataset: RadarDataset = {data: [], fill: false, label: "Current Score", borderColor:"#387AC4",borderWidth:1}
+              let targetDataset: RadarDataset = {data: [], fill: false, label: "Target Score", borderColor:"#1D3650",borderWidth:1}
               eachModule?.children?.forEach(eachTopic => {
                 radarChartData.data.labels?.push(eachTopic.name)
-                console.log(eachTopic.name, eachTopic.rating, eachTopic.value)
-                if (eachTopic.value != null && eachTopic.rating != null) {
-                  dataset.data.push(eachTopic.rating === 0 || eachTopic.rating ? eachTopic.value : eachTopic?.rating)
+                if (eachTopic.rating !== undefined) {
+                  dataset.data.push(eachTopic.value ? eachTopic.value : eachTopic.rating)
                   targetDataset.data.push(5)
                 }
               })
