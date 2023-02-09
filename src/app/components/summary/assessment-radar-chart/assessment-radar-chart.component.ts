a9fab0d043c65b9bb0745e6b7f5670c8fa5f5089
rampg23 <ramprakash.pg@thoughtworks.com>
2023-02-09 17:51:04 +0530

    [289][Ram|Sahiti] Cosemetic changes to radar chart

--- src/app/components/summary/assessment-radar-chart/assessment-radar-chart.component.ts
+++ src/app/components/summary/assessment-radar-chart/assessment-radar-chart.component.ts
@@ -8,12 +8,7 @@ interface RadarChartProperty {
   label: string,
   borderColor: string,
   borderWidth: number,
-  backgroundColor:string
-}
-
-interface RadarChart {
-  labels: string[],
-  datasets: RadarChartProperty[]
+  backgroundColor: string
 }
 
 interface RadarChartModuleData {
@@ -34,19 +29,34 @@ interface RadarChartData {
 export class AssessmentRadarChartComponent implements OnInit {
   radarChartData: RadarChartData[] = [];
   public radarChartType: ChartType = 'radar';
-  public radarChartOptions: ChartConfiguration['options']= {
+  public radarChartConfiguration: ChartConfiguration['options'] = {
+    plugins: {
+      tooltip: {
+        displayColors:false,
+        callbacks: {
+          title: (context) => {
+            return context[0].label
+          }
+        },
+      }
+    },
     elements: {
       line: {
         tension: 0.1,
       },
-      point:{
-        radius : 2,
+      point: {
+        radius: 2,
       },
     },
     scales: {
       r: {
         ticks: {
           stepSize: 1
+        },
+        pointLabels:{
+          callback:(label,index) =>{
+            return label.length > 15 ? label.substring(0, 15) + "..." : label
+          }
         },
         grid: {
           circular: true,
@@ -59,7 +69,6 @@ export class AssessmentRadarChartComponent implements OnInit {
 
   @Input()
   summaryData: ReportDataStructure
-
 
   constructor() {
   }
@@ -78,7 +87,7 @@ export class AssessmentRadarChartComponent implements OnInit {
                 moduleName: eachModule.name,
                 data: {labels: [], datasets: []}
               }
-              let dataset: RadarChartProperty = {
+              let currentData: RadarChartProperty = {
                 backgroundColor: 'red',
                 data: [],
                 fill: false,
@@ -86,7 +95,7 @@ export class AssessmentRadarChartComponent implements OnInit {
                 borderColor: 'red',
                 borderWidth: 1
               }
-              let targetDataset: RadarChartProperty = {
+              let targetData: RadarChartProperty = {
                 backgroundColor: 'green',
                 data: [],
                 fill: false,
@@ -95,14 +104,14 @@ export class AssessmentRadarChartComponent implements OnInit {
                 borderWidth: 1
               }
               eachModule?.children?.forEach(eachTopic => {
-                radarChartModuleData.data.labels?.push(eachTopic.name.length>15 ? eachTopic.name.substring(0,15)+ "..."   :eachTopic.name)
+                radarChartModuleData.data.labels?.push(eachTopic.name)
                 if (eachTopic.rating !== undefined) {
-                  dataset.data.push(eachTopic.value ? eachTopic.value : eachTopic.rating)
-                  targetDataset.data.push(5)
+                  currentData.data.push(eachTopic.value ? eachTopic.value : eachTopic.rating)
+                  targetData.data.push(5)
                 }
               })
-              radarChartModuleData.data.datasets.push(dataset)
-              radarChartModuleData.data.datasets.push(targetDataset)
+              radarChartModuleData.data.datasets.push(currentData)
+              radarChartModuleData.data.datasets.push(targetData)
               radarChartData.data.push(radarChartModuleData)
             }
           })
