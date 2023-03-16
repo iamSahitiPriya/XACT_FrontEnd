97b792de9ac0049183cde220bd50c50c4cd02d03
rampg23 <ramprakash.pg@thoughtworks.com>
2023-03-16 18:19:11 +0530

    [No-Card][Ram|Brindha] Fix. Change in admin console according to the route

--- src/app/components/admin/admin-console/admin-console.component.ts
+++ src/app/components/admin/admin-console/admin-console.component.ts
@@ -2,11 +2,12 @@
  * Copyright (c) 2022 - Thoughtworks Inc. All rights reserved.
  */
 
-import {Component} from '@angular/core';
+import {Component, OnChanges, OnDestroy} from '@angular/core';
 import {Router} from "@angular/router";
 import {Store} from "@ngrx/store";
 import {AppStates} from "../../../reducers/app.states";
 import * as fromActions from "../../../actions/assessment-data.actions";
+import {Subject, takeUntil} from "rxjs";
 
 
 @Component({
@@ -14,19 +15,26 @@ import * as fromActions from "../../../actions/assessment-data.actions";
   templateUrl: './admin-console.component.html',
   styleUrls: ['./admin-console.component.css']
 })
-export class AdminConsoleComponent {
+export class AdminConsoleComponent implements OnDestroy{
   type = "";
   tabIndex: number;
+  private destroy$: Subject<void> = new Subject<void>();
+
 
   constructor(private router: Router,private store: Store<AppStates>) {
-    const currentRoute = this.router.url.split('?')[0];
-    const path = currentRoute.split('/').pop()||'';
-    this.setEvent(path);
-    this.store.dispatch(fromActions.isAdmin({isAdmin:true}))
+    this.router.events.pipe(takeUntil(this.destroy$)).subscribe(_res =>{
+      const currentRoute = this.router.url.split('?')[0];
+      const path = currentRoute.split('/').pop()||'';
+      this.setEvent(path);
+      this.store.dispatch(fromActions.isAdmin({isAdmin:true}))
+    })
   }
 
   setEvent(name: string) {
     this.type = name;
   }
-
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
 }
