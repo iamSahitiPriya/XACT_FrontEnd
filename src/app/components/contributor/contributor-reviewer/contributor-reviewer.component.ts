85fd656b218e6395d3bbec59d4ffe8ad6f928b23
RUCHIKA MODGIL <ruchika.modgil@thoughtworks.com>
2023-04-06 14:01:34 +0530

    [358] [Ruchika | Brindha] Create Reviewer Component

---
+++ src/app/components/contributor/contributor-reviewer/contributor-reviewer.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-contributor-reviewer',
+  templateUrl: './contributor-reviewer.component.html',
+  styleUrls: ['./contributor-reviewer.component.css']
+})
+export class ContributorReviewerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
