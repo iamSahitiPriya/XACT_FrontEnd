2bfd8b0f8a4c8a94a469493fb4c4451695dcb93f
SahitiPriyaA <sahitipriya.a@thoughtworks.com>
2023-04-12 18:28:41 +0530

    [357] [Sahiti | Ram] Refactor contributor data and remove functionality

--- src/app/components/admin/manage-contributors/manage-contributors.component.ts
+++ src/app/components/admin/manage-contributors/manage-contributors.component.ts
@@ -3,10 +3,8 @@ import {MAT_DIALOG_DATA, MatDialog} from "@angular/material/dialog";
 import {COMMA, ENTER} from "@angular/cdk/keycodes";
 import {ContributorStructure} from "../../../types/Contributor/ContributorStructure";
 import {MatChipInputEvent} from "@angular/material/chips";
-import {AbstractControl, FormControl, UntypedFormBuilder, UntypedFormGroup, Validators} from "@angular/forms";
-import {log} from "util";
+import {FormControl, UntypedFormBuilder} from "@angular/forms";
 import {data_local} from "../../../messages";
-
 
 @Component({
   selector: 'app-manage-contributors',
@@ -17,20 +15,17 @@ export class ManageContributorsComponent implements OnInit {
   addOnBlur: boolean = true;
   emailPattern = /^([_A-Za-z\d-+]+\.?[_A-Za-z\d-+]+@(thoughtworks.com),?)*$/;
   readonly separatorKeysCodes = [ENTER, COMMA] as const;
-  emailTextField: string;
   authors: ContributorStructure[] = []
   reviewers: ContributorStructure[] = []
-  reviewerText: string;
   mandatoryFieldText = data_local.ASSESSMENT.MANDATORY_FIELD_TEXT;
   userEmailErrorMessage = data_local.ASSESSMENT.USER_EMAIL.ERROR_MESSAGE;
-
-
 
   @ViewChild("chipList1") chipList: any;
   authorFormControl = new FormControl(this.authors);
   contributorCtrl = new FormControl();
   reviewerFormControl = new FormControl(this.reviewers);
-
+  contributors = new Map<string, ContributorStructure[]>();
+  contributorFormControllers = [this.authorFormControl, this.reviewerFormControl];
 
   constructor(@Inject(MAT_DIALOG_DATA) public data: any, public dialog: MatDialog, private formBuilder: UntypedFormBuilder) {
   }
@@ -48,7 +43,10 @@ export class ManageContributorsComponent implements OnInit {
           this.reviewers.push(eachContributor)
         }
       })
+
     }
+    this.contributors.set('Author', this.authors);
+    this.contributors.set('Reviewer', this.reviewers);
   }
 
   addContributor(event: MatChipInputEvent, role: string) {
@@ -57,21 +55,28 @@ export class ManageContributorsComponent implements OnInit {
         userEmail: event.value,
         role: role
       }
-      if (contributor.role === 'AUTHOR') {
-        this.emailTextField = ""
-        this.authors.push(contributor)
-        this.authorFormControl.setValue(this.authors)
-        this.contributorCtrl.setValue(null)
-      } else if (contributor.role === 'REVIEWER') {
-        this.reviewerText = ""
-        this.reviewers.push(contributor)
-        this.reviewerFormControl.setValue(this.reviewers)
-        this.contributorCtrl.setValue(null)
-      }
+      this.contributors.get(role)?.push(contributor);
+      this.resetFormControl();
     }
   }
 
   closeDialog() {
     this.dialog.closeAll()
   }
+
+  removeContributor(userEmail: string, role: string): void {
+
+    const index = this.contributors.get(role)?.findIndex(contributor => contributor.userEmail === userEmail);
+    if (index !== undefined && index !== -1) {
+      this.contributors.get(role)?.splice(index, 1);
+    }
+    this.resetFormControl();
+  }
+
+  private resetFormControl() {
+    this.authorFormControl.setValue(this.authors)
+    this.reviewerFormControl.setValue(this.reviewers)
+    this.contributorCtrl.setValue(null)
+  }
+
 }
