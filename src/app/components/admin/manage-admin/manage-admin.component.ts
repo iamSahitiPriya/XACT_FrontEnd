21a5ed176f8fa432f50df8569c3636a0ce649f9e
rampg23 <ramprakash.pg@thoughtworks.com>
2023-05-10 17:32:03 +0530

    [408][Ram] | Validate input fields for duplicate users

--- src/app/components/admin/manage-admin/manage-admin.component.ts
+++ src/app/components/admin/manage-admin/manage-admin.component.ts
@@ -6,10 +6,11 @@ import {Component, OnInit} from '@angular/core';
 import {AppServiceService} from "../../../services/app-service/app-service.service";
 import {UserInfo} from "../../../types/UserInfo";
 import {Observable, startWith, Subject, takeUntil} from "rxjs";
-import {FormControl} from "@angular/forms";
+import {AbstractControl, FormControl, UntypedFormBuilder, UntypedFormGroup, Validators} from "@angular/forms";
 import {map} from "rxjs/operators";
 import {AccessControlRole} from "../../../types/AccessControlRole";
 import {MatTableDataSource} from "@angular/material/table";
+import {data_local} from "../../../messages";
 
 @Component({
   selector: 'app-manage-admin',
@@ -21,25 +22,45 @@ export class ManageAdminComponent implements OnInit {
   initialState: UserInfo[];
   userControl = new FormControl();
   users: UserInfo[];
-  accessControlRole:AccessControlRole[];
-  dataSource:MatTableDataSource<AccessControlRole>
+  accessControlRole: AccessControlRole[];
+  dataSource: MatTableDataSource<AccessControlRole>
   private destroy$: Subject<void> = new Subject<void>();
+  mandatoryFieldText = data_local.ASSESSMENT.MANDATORY_FIELD_TEXT;
+
   displayedColumns: string[] = ['name', 'role', 'action'];
+  addUserFormGroup: UntypedFormGroup;
+  public userEmailRoleValidator = {
+    'userFormControl': [
+      {type: 'invalidAutocompleteString', message: "Not found"},
+      {type: 'required', message: this.mandatoryFieldText},
+      {type: 'alreadyPresent', message: 'Already present'}
+    ]
+  }
+  userEmail: string;
 
 
-  constructor(private appService: AppServiceService) {
+  constructor(private appService: AppServiceService, private formBuilder: UntypedFormBuilder) {
     this.dataSource = new MatTableDataSource<AccessControlRole>(this.accessControlRole);
   }
 
   ngOnInit(): void {
+    this.addUserFormGroup = this.formBuilder.group(
+      {
+        userEmailRoleValidator: ['', Validators.required]
+      }
+    )
     this.appService.getLoggedInUserInfo().subscribe(data => {
       this.initialState = data
       this.filterUser();
     })
-    this.appService.getAccessControlRoles().subscribe(data =>{
+    this.appService.getAccessControlRoles().subscribe(data => {
       this.accessControlRole = data;
       this.dataSource = new MatTableDataSource(this.accessControlRole)
     })
+  }
+
+  get form(): { [key: string]: AbstractControl } {
+    return this.addUserFormGroup.controls;
   }
 
   private _filterUser(value: string): UserInfo[] {
@@ -61,6 +82,7 @@ export class ManageAdminComponent implements OnInit {
   }
 
   private filterUser() {
+    this.addUserFormGroup.controls['userEmailRoleValidator'].setValidators(this.autoCompleteValidator(this.users))
     this.filteredUsers = this.userControl.valueChanges.pipe(
       startWith(''),
       map((_state: string) => {
@@ -68,24 +90,49 @@ export class ManageAdminComponent implements OnInit {
       })
     )
   }
+
   onInputChange() {
     this.filterUser()
   }
 
-  saveRole(email: string, role:string) {
-    let roleRequest:AccessControlRole = {
-      email:email,
-      accessControlRoles:role
+  saveRole(email: string, role: string) {
+    if (this.addUserFormGroup.valid) {
+      let roleRequest: AccessControlRole = {
+        email: email,
+        accessControlRoles: role
+      }
+      let filteredData = this.dataSource.data.filter(eachData => eachData.email === roleRequest.email)
+      if (filteredData.length === 0) {
+        this.appService.saveRole(roleRequest).pipe(takeUntil(this.destroy$)).subscribe(
+          (_data) => {
+            this.accessControlRole.push(roleRequest)
+            this.dataSource.data = this.accessControlRole
+            this.userEmail = ""
+          })
+      }
+      // }else{
+      //   this.addUserFormGroup.controls['userEmailRoleValidator'].setValidators({'alreadyPresent':{value:{}}})
+      //
+      // }
     }
-    this.appService.saveRole(roleRequest).pipe(takeUntil(this.destroy$)).subscribe(
-      (_data)=>{
-        this.accessControlRole.push(roleRequest)
-        this.dataSource.data = this.accessControlRole
-    })
 
   }
+
   ngOnDestroy(): void {
     this.destroy$.next();
     this.destroy$.complete();
+  }
+
+  private autoCompleteValidator(users: UserInfo[]) {
+    let flag: boolean = false
+    return (control: AbstractControl): { [key: string]: any } | null => {
+      users.forEach(eachUser => {
+        if (eachUser.email == control.value) {
+          flag = true
+        }
+      })
+      console.log(flag)
+      return flag ? null : {'invalidAutocompleteString': {value: control.value}}
+    }
   }
 }
